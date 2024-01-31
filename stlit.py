5daf6a3c3c751bff19492baeec4b020d8b9283d9
GUYkanawat <153839909+GUYkanawat@users.noreply.github.com>
2024-01-31 14:59:58 +0700

    Update stlit.py

--- stlit.py
+++ stlit.py
@@ -3,7 +3,7 @@ import pandas as pd
 from streamlit_shap import st_shap
 import joblib
 import shap
-
+import streamlit_pandas as sp
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import  mean_absolute_error, mean_squared_error, r2_score
