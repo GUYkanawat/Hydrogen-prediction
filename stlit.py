ef7e86d3f1d62655590decabd54254d074deed19
GUYkanawat <153839909+GUYkanawat@users.noreply.github.com>
2024-03-13 23:12:45 +0700

    Update stlit.py

--- stlit.py
+++ stlit.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import  mean_absolute_error, mean_squared_error, r2_score
 from scipy.stats import pearsonr
-import seaborn as sns;sns.set()
 import numpy as np
 
 
